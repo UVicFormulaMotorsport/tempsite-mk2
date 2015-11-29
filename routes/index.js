1282d0d4a81c44d4dd6e34588339f1356cca9cf6
Jakob Roberts <jakob.m.roberts@gmail.com>
2015-11-29 14:21:38 -0800

    Add routing
    Add team page
    Add contact page
    Fix navbar

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,31 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'UVic FSAE' });
 });
 
+/* GET */
+router.get('/sponsorship/', function(req, res, next) {
+  res.render('sponsorship', { title: 'UVic FSAE | Contact Us' });
+});
+
+/* GET */
+router.get('/about/car/', function(req, res, next) {
+  res.render('car', { title: 'UVic FSAE | The Car' });
+});
+
+/* GET */
+router.get('/about/team/', function(req, res, next) {
+  res.render('team', { title: 'UVic FSAE | The Team' });
+});
+
+/* GET */
+router.get('/about/comp/', function(req, res, next) {
+  res.render('comp', { title: 'UVic FSAE | The Competition' });
+});
 
 
-
+/* GET */
+router.get('/contact/', function(req, res, next) {
+  res.render('contact', { title: 'UVic FSAE | Contact Us' });
+});
 
 
 module.exports = router;
